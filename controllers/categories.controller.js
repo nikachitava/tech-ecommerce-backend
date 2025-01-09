3071db4bc06011d302fa99dec35d03335748a63b
nikachitava <chitava18.official@gmail.com>
2025-01-09 20:58:00 +0400

    correct the name of file

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,4 +1,4 @@
-import { Category } from "../models/Category.module.js";
+import { Category } from "../models/Category.model.js";
 import { slugify } from "../utils/slug.js";
 
 export const getCategories = async (req, res) => {
