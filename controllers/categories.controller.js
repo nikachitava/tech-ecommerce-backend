f9afdc8b68fb589df521e81125ede8c2f892f734
nikachitava <chitava18.official@gmail.com>
2025-01-09 20:57:11 +0400

    refactor category model name

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,4 +1,4 @@
-import { Category } from "../models/Category.js";
+import { Category } from "../models/Category.module.js";
 import { slugify } from "../utils/slug.js";
 
 export const getCategories = async (req, res) => {
