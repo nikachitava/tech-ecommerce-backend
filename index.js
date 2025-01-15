efb9abec6903567945de5f4fd96c32934915bec3
nikachitava <chitava18.official@gmail.com>
2025-01-15 23:23:27 +0400

    update cors

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Credentials", true);
 	next();
 });
-app.use(cors({ origin: "http://localhost:5173" }));
+
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
